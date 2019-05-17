65b94357291fde1321b547c366bfbcfe15529559
jun <junseok.kim8707@gmail.com>
2019-05-17 02:28:50 -0400

    Second Commit - Mongoose Encryption added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const encrypt = require("mongoose-encryption");
 const app = express();
 
 app.use(express.static("public"));
@@ -14,10 +14,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisissecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields:["password"]} );
 
 const User = new mongoose.model("User", userSchema);
 
@@ -33,6 +36,7 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+//When the user register
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
@@ -48,6 +52,7 @@ app.post("/register", function(req, res){
   });
 });
 
+//When the user login
 app.post("/login", function(req, res){
   const username = req.body.username;
   const password = req.body.password;
